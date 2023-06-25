3dbc4a68d668a60c9b0251660fd3f1cbd75521bb
Akram Ali <akram.exp02@gmail.com>
2023-06-25 16:38:20 +0530

    temp nd sky added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_date
 
 st.title("Weather Forcast for Upcoming Days")
 place = st.text_input("Place: ")
@@ -7,3 +9,19 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for next {days} days in {place}")
+
+if place:
+    filtered_data = get_date(place, days)
+
+    if option == "Temperature":
+        temperature = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width=115)
